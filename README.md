a1a189ffeedeee50887a7107406b58a05eb26868
Gil <gilberto.gape@gmail.com>
2021-11-26 21:34:31 -0500

    Newest commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # python_github_first
 
 This is an example to demonstrate the use of git on Ubuntu.
+
+Make a change
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 
 def main(msg):
+    # Another comment
     # Adding the second comment to this file
     # Print a message
     print(msg)
